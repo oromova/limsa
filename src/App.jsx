de42b44b351c83f5547bebdb821ee1f425ea2066
Yulduz_Oromova <mahmurbekovna@gmail.com>
2025-02-11 11:17:16 +0500

    Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Footer } from './Components/Footer';
 import { Header } from './Components/Header';
 import { Outlet } from 'react-router-dom';
+import Loading from './Components/Loading';
 
 export const App = () => {
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
     <div>
-      <Header/>
-      <Outlet/>
-      <Footer/>
+      {loading ? <span><Loading/></span> :
+      <div>
+        <Header/>
+        <Outlet/>
+        <Footer/>
+      </div>
+      }
     </div>
   );
 };
